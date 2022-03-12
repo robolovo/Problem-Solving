f1441e505c727f875567dffcb9cfc047ed2b33d4
robolovo <dlwnstj21@naver.com>
2022-03-12 19:20:23 +0900

    20220312

--- src/main/java/problem/BOJ/BOJ_16724.java
+++ src/main/java/problem/BOJ/BOJ_16724.java
@@ -1,54 +1,70 @@
 package problem.BOJ;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BOJ_16724 {
 
+    static int N, M;
     static char[][] map;
-    static boolean[][] visited;
+    static int[] parents;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] s = sc.nextLine().split(" ");
-        int N = Integer.parseInt(s[0]);
-        int M = Integer.parseInt(s[1]);
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         map = new char[N][M];
-        visited = new boolean[N][M];
+        parents = new int[N * M];
+
+        for (int i = 0; i < parents.length; i++) {
+            parents[i] = i;
+        }
 
         for (int i = 0; i < N; i++) {
-            String input = sc.nextLine();
-            for (int j = 0; j < M; j++) {
-                map[i][j] = input.charAt(j);
-            }
+            map[i] = br.readLine().toCharArray();
         }
 
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                calc(i, j);
-            }
-        }
-
+//        for (int i = 0; i < N; i++) {
+//            for (int j = 0; j < M; j++) {
+//                int curIdx = i * M + j;
+//                int nextIdx = findIdx(i, j);
+//                if (find(curIdx) != find(nextIdx)) {
+//                    union(curIdx, nextIdx);
+//                }
+//            }
+//        }
+//
+//        int answer = findSaveZone();
+//        System.out.println(answer);
     }
 
-    public static void calc(int x, int y) {
-        int startX = x;
-        int startY = y;
 
 
-        visited[x][y] = true;
-        if (map[x][y] == 'U') {
-            x -= 1;
-        } else if (map[x][y] == 'D') {
-            x += 1;
-        } else if (map[x][y] == 'L') {
-            y -= 1;
-        } else {
-            y += 1;
-        }
-    }
+//    static int rows;
+//    static int cols;
+//    static char[][] map;
+//    static boolean[][] visit;
+//    static int answer = 0;
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//        rows = Integer.parseInt(st.nextToken());
+//        cols = Integer.parseInt(st.nextToken());
+//        map = new char[rows][cols];
+//        visit = new boolean[rows][cols];
+//        for (int i = 0; i < rows; i++) {
+//            String line = br.readLine();
+//            for (int j = 0; j < cols; j++) {
+//                map[i][j] = line.charAt(j);
+//            }
+//        }
+//
+//
+//
+//    }
 
 }
